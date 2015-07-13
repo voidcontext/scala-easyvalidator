674255b2f6efe020042b4cbac95a8008161cbd8f
Gabor Pihaj <gabor.pihaj@gmail.com>
2015-07-13 19:35:47 +0100

    Add travis.yml + crossbuild setting

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,8 @@ object EasyValidatorBuild extends Build {
       name := "easyvalidator",
       organization := "io.github.voidcontext",
       version := "0.1.0",
-      scalaVersion := "2.11.6",
+      scalaVersion := "2.11.0",
+      crossScalaVersions := Seq("2.10.0", "2.11.0"),
       scalacOptions := Seq("-feature", "-deprecation"),
       libraryDependencies ++= Seq(
         "org.scalatest" %% "scalatest" % "2.2.5" % "test"
